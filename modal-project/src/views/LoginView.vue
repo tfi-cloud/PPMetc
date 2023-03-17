78561308cf1449b96c979035c2b45262707183df
zarek-l <carolina.mendoza@epn.edu.ec>
2023-03-17 12:50:29 -0500

    Add route login

---
+++ modal-project/src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <main-bar></main-bar>
+    <sign-up-form></sign-up-form>
+  </div>
+</template>
+
+<script>
+
+import SignUpForm from '../components/SignUpForm.vue'
+import MainBar from '../components/MainBar.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    SignUpForm,
+    MainBar
+  }
+}
+</script>
