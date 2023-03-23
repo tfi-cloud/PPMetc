fa0cae159eb8bf802674e44403cf83fa327ae251
zarek-l <carolina.mendoza@epn.edu.ec>
2023-03-22 23:37:03 -0500

    Add WelcomeBar

--- modal-project/src/components/WelcomeBar.vue
+++ modal-project/src/components/WelcomeBar.vue
@@ -1,48 +1,75 @@
 <template>
-    <div class="main-nav">
-        <button>Get your plan</button>
+    <div class="welcome-nav">
+        <div class="welcome">
+            <img class="logo" src="@/assets/TFI-Cloud.png"/> 
+        </div>
+        <div class="user">
+            <a>Welcome </a>
+            <a class="username">user</a>
+        </div>
+        <div class="button">
+            <button>Get your plan</button>
+        </div>
+        
     </div>
+
   </template>
   
   <script>
   export default {
   }
   </script>
+
   
-  <style escoped>
-  
-    div .main-nav{
-      max-width: 100%;
-      height: 60px;
-      background-color: #1571C1;
-      display: flexbox;
-      margin-bottom: 40px;
-      column-count: 2;
-      border-bottom-style: solid !important;
-      border-bottom-color: #FEAE00;
+  <style scoped>
+  div .welcome-nav{
+        width: 100%;
+        height: 70px;
+        background-color: #1571C1;
+        display: flex;
+        align-items: flex-start;
+        column-count: 3;
+        position: fixed;
     }
     button {
-      border:none;
-      background-color:#FEAE00;
-      color:black;
-      font-weight: bold;
-      max-width: 60%;
-      padding: 10px 25px;
-      margin-top: 10px;
-      border-radius: 5px;
-      margin-right: 3%;
-      display: inline-flexbox;
-      cursor: pointer;
+        border:none;
+        background-color:#FEAE00;
+        color:black;
+        font-weight: bold;
+        width: 90%;
+        max-width: 100%;
+        padding: 5px 25px;
+        box-sizing: border-box;
+        border-radius: 5px;
+        margin-left: 20%;
+        cursor: pointer;
     }
-    .buttons{
-      margin-left: 50%;
+    .button{
+        margin-top: 4px;
+        margin-left: 70%;
     }
     .logo{
-      max-width: 110px;
-      max-height: auto; 
-      margin-top: 4px;
-      cursor: pointer;
-      margin-right: 70%;
+        max-width: 110px;
+        max-height: auto; 
+        margin-top: 7px;
+        cursor: pointer;
+        margin-left: 30%;
+        display: block;
     }
-  
+    .user{
+        margin-top: 25px;
+        cursor: pointer;
+        margin-left: 4%;
+    }
+    a{
+        color:white;
+        letter-spacing: 1px;
+        text-align: center;
+        margin: 0 auto;
+    }
+    a.username{
+        font-weight: bold;
+        font-size: 0.9em;
+        text-transform: capitalize;
+    }
   </style>
