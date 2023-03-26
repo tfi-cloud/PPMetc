587e63938ee949e21adf1bfb65b24fef81a4d89a
zarek-l <carolina.mendoza@epn.edu.ec>
2023-03-25 19:14:19 -0500

    Add carousel dependency

--- modal-project/src/views/DashboardView.vue
+++ modal-project/src/views/DashboardView.vue
@@ -11,7 +11,15 @@
           <title>Portfolio 1</title>
 
           <div class="carousel">
-
+            <vueper-slides class="no-shadow" :visible-slides="3" slide-multiple :bullets="false" :gap="3" :slide-ratio="1 / 4"  :dragging-distance="200" :breakpoints="{ 500: { visibleSlides: 2, slideMultiple: 2 } }">
+              <vueper-slide
+                v-for="(slide, i) in slides"
+                :key="i"
+                :title="slide.title"
+                :content="slide.content"
+                :style="'background-color: white'">
+              </vueper-slide>
+            </vueper-slides>
           </div>
 
         </div>
@@ -32,19 +40,54 @@
     import WelcomeBar from '../components/WelcomeBar.vue'
     import SearchBar from '../components/SearchBar.vue'
     import NavBar from '../components/NavBar.vue'
-  
+    import { VueperSlides, VueperSlide } from 'vueperslides'
+    import 'vueperslides/dist/vueperslides.css'
+
     export default {
       name: 'DashboardView',
       components: {
         WelcomeBar,
         SearchBar,
         NavBar,
+        VueperSlides,
+        VueperSlide,
       },
       data(){
         return{
-            showDemo:true,
+            
         }
     },
+    
+data: () => ({
+  slides: [
+    {
+      title: 'Slide #1',
+      content: 'Slide 1 content.'
+    },
+    {
+      title: 'Slide #2',
+      content: 'Slide 2 content.'
+    },
+    {
+      title: 'Slide #3',
+      content: 'Slide 3 content.'
+    },
+    {
+      title: 'Slide #4',
+      content: 'Slide 4 content.'
+    },
+    {
+      title: 'Slide #5',
+      content: 'Slide 5 content.'
+    },
+    {
+      title: 'Slide #6',
+      content: 'Slide 6 content.'
+    },
+
+  ],
+  showDemo:true,
+}),
     methods:{
         toggleShowDemo(){
             this.showDemo = false
@@ -61,17 +104,17 @@
     width: 100%;
     height: 100%;
     position: relative;
-    padding: 10px;
+    padding: 5px;
     grid-gap: 10px;
     background-color: white;
     flex-grow: 1;
   }
-  .canvas{
-    width: 85%;
+  .dashboard .canvas{
+    width: 90%;
     margin: 10px;
     background: #EFEFEF;
     text-align: left;
-    padding: 40px;
+    padding: 20px;
     border-radius: 10px; 
     grid-row: 1 / 5;
     margin-left: 5%;
@@ -100,12 +143,19 @@
     border: 1px solid #B9B9B9;
     background: #EFEFEF;
   }
-  button:hover{
+  .welcome button:hover{
     background-color:#ECA60E;
   }
-  .cancel:hover{
+  .welcome .cancel:hover{
     background-color:#D9D9D9; 
   }
-
-
+  .vueperslides__arrow{
+    color: #4C9CCB;
+    stroke-width: 1px;
+  }
+  .vueperslides__arrow:hover{
+    color: #ECA60E;
+    stroke-width: 1px;
+    background-color: transparent;
+  }
 </style>
