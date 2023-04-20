fabacc64e51a77dc433597ce13c9c5c7221a8e57
zarek-l <carolina.mendoza@epn.edu.ec>
2023-04-20 17:25:30 -0500

    Add component update portfolio

---
+++ modal-project/src/components/UpdatePortfolio.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="modalSave">
+        <div class="update">
+            <span class="material-icons" style="font-size: 30px;">check_circle</span> 
+            <title>Your changes have been saved.</title>
+        </div>
+        <span class="material-icons" @click.capture="closeModal">clear</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      visible: true
+    }
+  },
+    methods: {
+      closeModal() {
+        this.$emit('close')
+      }
+    }
+}
+</script>
+
+<style scoped>
+    .modalSave{
+        display: flex;
+        align-items: center;
+        top: 0;
+        left: 31.5%;
+        position: fixed;
+        z-index: 1;
+        width: 500px;
+        height: 30px;
+        padding: 14px;
+        margin: 230px auto;
+        background: #2E844A;
+        border-radius: 5px; 
+    }
+    .update {
+        display: flex;
+        align-items: center;
+        flex: 1;
+    }
+    .modalSave title{
+        color: white;
+        font-size: 1.6ch;
+        text-transform: initial;
+        text-align: center;
+        margin: auto 0px;
+        margin-left: 20px;
+    }
+    .modalSave .update{
+        flex: 1;
+    }
+    .modalSave span{
+        color: white;
+        cursor: pointer;
+    }
+</style>
--- modal-project/src/views/PortfolioDetails.vue
+++ modal-project/src/views/PortfolioDetails.vue
@@ -13,7 +13,11 @@
         <action-bar type="Add Portfolio" icon="add" @click.capture="toggleModal"/>
 
         <action-bar type="Delete Portfolio" icon="delete"/>
-        <action-bar type="Save Portfolio" icon="save"/>
+
+        <action-bar type="Save Portfolio" icon="save" @click.capture="toggleModalSave"/>
+        <div v-if="showSave">
+          <update-portfolio  @close="toggleModalSave"/>
+        </div>
       </div>
 
       <div class="container">
@@ -24,16 +28,20 @@
             <a class="title">{{ document.namePortfolio }}</a>
 
             <label>Name Portfolio</label>
-            <span disabled> {{ document.namePortfolio }}</span>
+            <span contenteditable id="NameField" @input="onNameInput" :class="{ edit: nameEdit }"> {{ document.namePortfolio }}</span>
 
             <label>Description</label>
-            <span disabled style="height: 90px; max-height: fit-content;"> {{ document.descriptionPortfolio }}</span>
+            <span contenteditable style="height: 90px; max-height: fit-content;" id="DescriptionField" @input="onDescriptionInput" :class="{ edit: descriptionEdit }"> {{ document.descriptionPortfolio }}</span>
 
             <label>Status</label>
-            <span disabled style="width: 40%;"> {{ document.selectedStatus }}</span>
+            <select v-model="statusProject" style="width: 40%; margin-bottom: 20px; padding: 10px; border-color: #DDDDDD; border-radius: 4px; font-size: medium; color: #6B6464;" id="StatusField" @change="onStatusInput" :class="{ edit: statusEdit }" class="portfolio-select">
+              <option disabled value="option">{{ document.selectedStatus }}</option>
+              <option value="active">Active</option>
+              <option value="disabled">Disabled</option>
+            </select>
 
             <label>Owner</label>
-            <span disabled style="width: 40%;"> {{ document.ownerPortfolio }}</span>
+            <span contenteditable style="width: 40%;" id="OwnerField" @input="onOwnerInput" :class="{ edit: ownerEdit }"> {{ document.ownerPortfolio }}</span>
 
             <label>Created by</label>
             <span disabled style="width: 40%; background-color: #EFEFEF;"> {{ document.createdBy }}</span>
@@ -105,6 +113,7 @@
     import getGoals from '../composables/getGoals'
     import SubNav from '../components/SubNav.vue'
     import getProjects from '../composables/getProjects'
+    import UpdatePortfolio from '../components/UpdatePortfolio.vue'
 
 export default {
   props:['id'],
@@ -118,6 +127,7 @@ export default {
       Portfolio,
       NewGoal,
       SubNav,
+      UpdatePortfolio,
     },
     setup(props){
       const { error, document } = getDocument('portfolios', props.id )
@@ -130,8 +140,14 @@ export default {
         heading: 'Create a new Portfolio',
         header: 'Create a new Goal',
         showModal: false,
+        showSave:false,
         showModalProject: false,
         showModalGoal:false,
+        nameEdit: false,
+        descriptionEdit: false,
+        ownerEdit:false,
+        statusEdit:false,
+        statusProject:'option',
       } 
     },
 
@@ -139,118 +155,153 @@ export default {
     toggleModal() {
       this.showModal = !this.showModal
     },
+    toggleModalSave() {
+      this.showSave = !this.showSave
+    },
     toggleModalProject() {
       this.showModalProject = !this.showModalProject
     },
     toggleModalGoal() {
       this.showModalGoal = !this.showModalGoal
+    },
+    onNameInput() {
+      this.nameEdit = true;
+    },
+    onDescriptionInput() {
+      this.descriptionEdit = true;
+    },
+    onOwnerInput() {
+      this.ownerEdit = true;
+    },
+    onStatusInput() {
+      this.statusEdit = true;
     }
+
   }
 }
 
 </script>
 
 <style scoped>
-    .container{
-      display: grid;
-      grid-template-columns: 16% 60% 30%;
-      max-width: 100%;
-      height: 100%;
-      position: relative;
-      padding: 10px;
-      grid-gap: 3px;
-    }
-    .canvas{
-      background: white;
-      text-align: left;
-      padding: 40px;
-      border-radius: 10px;
-      height: 440px;
-      overflow: auto;
-    }
-    .details{
-      display: grid;
-      grid-template-columns: auto 1fr;
-      grid-column-gap: 30px;
-      grid-row-gap: 0px;
-    }
-    .details .title{
-      margin-bottom: 50px;
-      color: #0071C1;
-      font-size: 20px;
-    }
-    .details label{
-      color:#646464;
-      font-size: 0.9em;
-      text-transform: capitalize;
-      letter-spacing: 1px;
-      font-weight: bold;
-      grid-column: 1 / 2;
-    }
-    .details span{
-      padding: 10px 5px 10px 5px;
-      width: 80%;
-      box-sizing: border-box;
-      border:none;
-      height: fit-content;
-      border: 1px solid #ddd;
-      color:#646464;
-      background-color: white;
-      margin-bottom: 3%;
-      grid-column: 2 / 3;
-      border-radius: 5px;
-    }
-    .header{
-      display: grid;
-      grid-template-columns: 68% 20%;
-      margin-top: 40px;
-    }
-    .header a{
-      text-transform: capitalize;
-      font-weight: bold;
-    }
-    .header hr{
-      margin-left: 0%;
-      margin-right: 50%;
-      width: 28%;
-      border: none;
-      height: 3px;
-      background-color: #ECA60E;
-    }
-    .add-objective{
-      width: 142px;
-      border: 1px solid #D9D9D9!important;
-      border-radius: 4px;
-      box-shadow: 2px 2px 2px rgba(184, 184, 184, 0.3);
-    }
-    .list-obj{
-      margin-top: 40px;
-      text-transform: capitalize;
-    }
-    .related{
-      width: 300px;
-      margin-left: 10px;
-      background: white;
-      text-align: left;
-      padding: 20px;
-      border-radius: 10px;
-      height: 260px;
-      overflow: auto;
-    }
-    .resume{
-      display: grid;
-      grid-template-columns: 16% 80%;
-    }
-    .related span{
-      display: block;
-      padding: 10px;
-      cursor: pointer;
-    }
-    .related a{
-      display: block;
-      padding: 10px;
-      margin-top: 5px;
-      text-transform: capitalize;
-      cursor: pointer;
-    }
+  .container{
+    display: grid;
+    grid-template-columns: 16% 60% 30%;
+    max-width: 100%;
+    height: 100%;
+    position: relative;
+    padding: 10px;
+    grid-gap: 3px;
+  }
+  .canvas{
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+    height: 440px;
+    overflow: auto;
+  }
+  .details{
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-column-gap: 30px;
+    grid-row-gap: 0px;
+  }
+  .details .title{
+    margin-bottom: 50px;
+    color: #0071C1;
+    font-size: 20px;
+  }
+  .details label{
+    color:#646464;
+    font-size: 0.9em;
+    text-transform: capitalize;
+    letter-spacing: 1px;
+    font-weight: bold;
+    grid-column: 1 / 2;
+  }
+  .details span{
+    padding: 10px 5px 10px 10px;
+    width: 80%;
+    box-sizing: border-box;
+    border:none;
+    height: fit-content;
+    border: 1px solid #ddd;
+    color:#646464;
+    background-color: white;
+    margin-bottom: 3%;
+    grid-column: 2 / 3;
+    border-radius: 5px;
+  }
+  .header{
+    display: grid;
+    grid-template-columns: 68% 20%;
+    margin-top: 40px;
+  }
+  .header a{
+    text-transform: capitalize;
+    font-weight: bold;
+  }
+  .header hr{
+    margin-left: 0%;
+    margin-right: 50%;
+    width: 28%;
+    border: none;
+    height: 3px;
+    background-color: #ECA60E;
+  }
+  .add-objective{
+    width: 142px;
+    border: 1px solid #D9D9D9!important;
+    border-radius: 4px;
+    box-shadow: 2px 2px 2px rgba(184, 184, 184, 0.3);
+  }
+  .list-obj{
+    margin-top: 40px;
+    text-transform: capitalize;
+  }
+  .related{
+    width: 300px;
+    margin-left: 10px;
+    background: white;
+    text-align: left;
+    padding: 20px;
+    border-radius: 10px;
+    height: 260px;
+    overflow: auto;
+  }
+  .resume{
+    display: grid;
+    grid-template-columns: 16% 80%;
+  }
+  .related span{
+    display: block;
+    padding: 10px;
+    cursor: pointer;
+  }
+  .related a{
+    display: block;
+    padding: 10px;
+    margin-top: 5px;
+    text-transform: capitalize;
+    cursor: pointer;
+  }
+  span.edit {
+    background-color: #FEF4DF; /* background when edited */
+  }
+  select.edit {
+    background-color: #FEF4DF; /* background when edited */
+  }
+  .details span:focus{
+    background-color: white;
+    border-color: #1B96FF;
+    border-width: 2px;
+    outline: none; 
+  }
+  .details .portfolio-select:focus{
+    background-color: white;
+    border-color: #1B96FF !important;
+    border-width: 2px !important;
+    outline: none !important; 
+  }
+
 </style>
